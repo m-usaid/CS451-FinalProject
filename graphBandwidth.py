7c60910aabbcb98e833524bdbac178990c5b244c
M. Usaid Rehman <mr04302@st.habib.edu..pk>
2021-05-25 22:18:27 +0500

    doc added for graphbandwidth

--- graphBandwidth.py
+++ graphBandwidth.py
@@ -12,9 +12,14 @@ class graphBandwidth(EA_imp):
                         mutation_rate, iterations, parent_ss, survivor_ss)
 
     def generate_individual(self):
+        """Create a single chromosome. 
+
+        Returns:
+            dict: A mapping of vertices to integers in the range {1,...,len(Graph)}
+        """        
         vertices = list(self.Graph.nodes())
         v_dict = {i:0 for i in vertices}
-        # e_dict = {i:0 for i in edges}
+        # randomize vertices to generate random numberings        
         shuff_vert = copy.deepcopy(vertices) 
         random.shuffle(shuff_vert)
         for key in v_dict.keys():
@@ -22,23 +27,43 @@ class graphBandwidth(EA_imp):
         return v_dict
 
     def fitness_function(self, chromosome: dict) -> int:
+        """Evaluate fitness of a given chromosome.
+
+        Args:
+            chromosome (dict): The chromosome to be evaluated. 
+
+        Returns:
+            int: The fitness value of the chromosome. 
+        """        
         edges = self.Graph.edges()
         e_dict = {i:0 for i in edges}
         for edge in e_dict:
+            # fit_ij = |f(i) - f(j)| forall ij in E(G)
             x = abs(int(chromosome[edge[0]]) - int(chromosome[edge[1]]))
             e_dict[edge] = x
+        # the longest mapped edge is fitness value 
         return max(e_dict.values())
 
     def init_population(self):
+        """Create gene pool by generating individuals in a loop.
+        """        
         self.population = []
         # genes = []
         for _ in range(self.population_size):
             chromosome = self.generate_individual()
-            # genes.append()
+            # append (chromosome, fitness) to genepool 
             self.population.append((chromosome, self.fitness_function(chromosome)))
-        # return population
 
-    def createChildfromNum(self, numbering):
+    def createChildfromNum(self, numbering: list) -> dict:
+        """Generate an individual chromosome from given numbering
+
+        Args:
+            numbering (list): the corresponding numbering to generate 
+            offspring from. 
+
+        Returns:
+            dict: The child chromosome. 
+        """        
         vertices = list(self.Graph.nodes())
         child = {i:0 for i in vertices}
         for key in child.keys():
@@ -46,14 +71,11 @@ class graphBandwidth(EA_imp):
         return child 
 
     def crossover(self, parent1: dict, parent2: dict):
-        """[summary]
+        """Perform 2-point crossover on 2 parents and return 2 children
 
         Args:
-            parent1 (dict): [description]
-            parent2 (dict): [description]
-
-        Returns:
-            [type]: [description]
+            parent1 (dict): The first parent.
+            parent2 (dict): The second parent. 
         """                       
         childAP1 = []
         childAP2 = []
@@ -61,14 +83,16 @@ class graphBandwidth(EA_imp):
         childBP2 = []
         child1 = []
         child2 = []
+        # the ordered numbering of the two parents 
         numberingA = list(parent1.values())
-        numberingB = list(parent1.values())
+        numberingB = list(parent2.values())
         size = len(numberingA)
         geneA = random.randint(0, size-1)
         geneB = random.randint(0, size-1)
+        # find randomized crossover points 
         startgene = min(geneA, geneB)
         endgene = max(geneA, geneB)
-        # print(startgene, endgene)
+        # store parts of gene to create offspring 
         for i in range(startgene, endgene):
             childAP1.append(numberingA[i])
             childBP1.append(numberingB[i])
@@ -76,16 +100,21 @@ class graphBandwidth(EA_imp):
         childBP2 = [item for item in numberingB if item not in childBP1]
         child1 = childAP2[:startgene] + childAP1 + childAP2[startgene:]
         child2 = childBP2[:startgene] + childBP1 + childBP2[startgene:]
+        # create offspring from given numberings 
         offspring1 = self.createChildfromNum(child1)
         offspring2 = self.createChildfromNum(child2)
         return offspring1, offspring2
 
-    def mutation_operator(self, individual: dict):
-        """[summary]
+    def mutation_operator(self, individual: dict) -> dict:
+        """Perform mutation on a chromosome by swapping the numbering 
+        of two vertices randomly based on mutation rate. 
 
         Args:
-            individual (dict): [description]
-        """        
+            individual (dict): the individual on whom mutation is being applied.
+
+        Returns:
+            dict: mutated individual. 
+        """            
         random.seed()
         randNum = random.random()
         if randNum < self.mutation_rate:
